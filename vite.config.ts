f7a20d657bee12b7aa1ac0df0bd5b8267dda0e33
Nya Reh <nya.reh@kbzbank.com>
2026-01-20 10:23:28 +0630

    fix spa

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
-import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   base: '/los-prototype/',
@@ -14,7 +14,9 @@ const config = defineConfig({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    tanstackStart(),
+    tanstackStart({
+      spa:{enabled: true},
+    }),
     viteReact(),
   ],
 })
